03b4ffea3306fe2129b706a0b976061e56f26db0
Jzekrlos <138406874+Jzekrlos@users.noreply.github.com>
2024-10-23 19:58:11 -0600

    Add files via upload

---
+++ Examen_I_POO/Productos/src/Producto.java
@@ -0,0 +1,17 @@
+public class Producto {
+    protected string nombre;
+    protected double precio;
+    protected int stock;
+    
+    public Producto() {
+    }
+
+    public Producto(string nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+   
+    
+
+}
